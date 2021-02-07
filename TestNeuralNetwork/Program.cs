607768f977a6737c83283f09f1b5da739d01d809
lionel@lionel-pc2 <lionel.desorme@free.fr>
2021-02-07 11:39:11 +0100

    Initial commit

---
+++ TestNeuralNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestNeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
